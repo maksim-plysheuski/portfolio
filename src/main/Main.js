3bc12dac15d2bdcb4d91ae7c7bd3298d29bbfa81
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-02-14 21:00:35 +0400

    main page sass refactoring

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "./Main.module.css"
+import style from "./Main.module.scss"
 import photo from "../assets/image/profilePhoto.png"
 import resume from "../assets/pdf/resume.jpg"
 import {FaLinkedin, FaGithub} from 'react-icons/fa';
@@ -10,27 +10,26 @@ function Main() {
     return <div className={style.mainBlock}>
         <div className={style.mainBlockContainer}>
             <div className={style.leftSide}>
-                <div className={style.iconsBlock}>
-
+                <div className={style.socialIconsBlock}>
                     <a href="https://www.linkedin.com/in/plysheuski-maksim" target="_blank" rel="noreferrer">
-                        <FaLinkedin className={style.socialIcon} />
+                        <FaLinkedin className={style.icon} />
                     </a>
                     <a href="https://github.com/maksim-plysheuski" target="_blank" rel="noreferrer">
-                        <FaGithub className={style.socialIcon}/>
+                        <FaGithub className={style.icon}/>
                     </a>
                     <a href="https://www.codewars.com/users/maksfilms" target="_blank" rel="noreferrer">
-                        <SiCodewars className={style.socialIcon}/>
+                        <SiCodewars className={style.icon}/>
                     </a>
                 </div>
             </div>
-            <img className={style.profilePhoto} src={photo}/>
+            <img className={style.profilePhoto} src={photo} alt="profile-photo"/>
             <div className={style.rightSide}>
                 <div className={style.rightSideContent}>
                     <h6>Frontend React Developer</h6>
                     <h1>Maksim Plysheuski</h1>
                     <p>JavaScript has made space in all companies irrespective of whether they are big or small. It is convenient and valuable and can create interactive applications and web pages for customers.</p>
                     <div className={style.buttonsContainer}>
-                        <a href={resume} download='resume' target='_blank'rel='noreferrer'>
+                        <a href={resume} download='resume' target='_blank' rel='noreferrer'>
                             <button className={style.resumeBtn}>Download CV</button>
                         </a>
                     </div>
