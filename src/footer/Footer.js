dd9cab3374be6ba802c75cff1f1bfaed411f3bdf
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2023-02-21 00:42:16 +0400

    footer final version

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,21 +1,12 @@
 import React from "react";
-import style from "./Footer.module.css"
-import containerStyle from "../common/styles/container.module.css"
+import style from "./Footer.module.scss"
+
 
 
 export const Footer = () => {
     return (
-        <footer className={style.footerBlock}>
-            <div className={`${style.footerContainer} ${containerStyle.container}`}>
-                <span>Maksim Plysheuski</span>
-                <div className={style.iconsBlock}>
-                    <div className={style.icon}></div>
-                    <div className={style.icon}></div>
-                    <div className={style.icon}></div>
-                    <div className={style.icon}></div>
-                </div>
-                <span>2022 © All rights reserved</span>
-            </div>
+        <footer className={style.footer}>
+            <p>Made with<span> ❤ </span>by Maksim Plysheuski</p>
         </footer>
     )
 }
